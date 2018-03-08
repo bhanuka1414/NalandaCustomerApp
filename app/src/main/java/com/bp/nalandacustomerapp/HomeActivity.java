89d292a1d20fec57ffc88f9f097a2a9eecb6c192
Heshan <heshanwijerathne@gmail.com>
2018-03-08 23:12:55 +0530

    design changes

--- app/src/main/java/com/bp/nalandacustomerapp/HomeActivity.java
+++ app/src/main/java/com/bp/nalandacustomerapp/HomeActivity.java
@@ -126,10 +126,6 @@ public class HomeActivity extends AppCompatActivity
 
         }else if (id == R.id.nav_message) {
 
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
-
         }
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
